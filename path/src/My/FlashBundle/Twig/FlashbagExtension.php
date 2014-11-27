4b4e7691d7c1e992a16ee42e5f5333cb480912fb
zogs <guichardsim@gmail.com>
2014-11-27 12:20:15 +0100

    swifswif

--- path/src/My/FlashBundle/Twig/FlashbagExtension.php
+++ path/src/My/FlashBundle/Twig/FlashbagExtension.php
@@ -15,34 +15,31 @@ class FlashbagExtension extends \Twig_Extension
 		$this->container = $container;
 		$this->flashbag = $container->get('flashbag');
 	}
- 
-
- 	public function renderAll($container = false)
- 	{
- 		$notifications = $this->flashbag->all();
-
- 		if( count($notifications)>0 ){
- 			return $this->container->get('templating')->render(
- 				"MyFlashBundle:Notifications:all.html.twig",
- 				compact("notifications","container")
- 				);
- 		}
- 		return null;
- 	}
 
 
- 	public function getFunctions()
-    {
-        return array(
-            'flashbag_all' => new \Twig_Function_Method($this, 'renderAll', array('is_safe' => array('html')))            
-        );
-    }
+	public function renderAll($container = false)
+	{
+		$notifications = $this->flashbag->all();
+
+		if( count($notifications)>0 ){
+			return $this->container->get('templating')->render("MyFlashBundle:Notifications:all.html.twig",compact("notifications","container"));
+		}
+		return null;
+	}
 
 
-    public function getName()
-    {
-        return 'my_flashbag_extension';
-    }
+	public function getFunctions()
+	{
+		return array(
+		'flashbag_all' => new \Twig_Function_Method($this, 'renderAll', array('is_safe' => array('html')))            
+		);
+	}
 
- 
+
+	public function getName()
+	{
+		return 'my_flashbag_extension';
+	}
+
+
 }
